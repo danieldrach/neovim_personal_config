746b334e1f4aa1fb72be3ef565f654e42f2d67e6
danieldrach <daniel.drach@gmail.com>
2024-12-02 22:16:56 +0000

    Added tab to change between open buffers.

--- lua/daniel/remap.lua
+++ lua/daniel/remap.lua
@@ -8,5 +8,5 @@ vim.api.nvim_set_keymap('n', '<Up>', '<C-w>k', { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<Down>', '<C-w>j', { noremap = true, silent = true })  -- Move to the window below
 vim.api.nvim_set_keymap('n', '<Left>', '<C-w>h', { noremap = true, silent = true })  -- Move to the window on the left
 vim.api.nvim_set_keymap('n', '<Right>', '<C-w>l', { noremap = true, silent = true }) -- Move to the window on the right
-vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true }) -- this is to exit terminal insert mode when it is in a separate window
 
