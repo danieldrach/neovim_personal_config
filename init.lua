67b70c5bb0d109b717060c5b8721832e34708587
danieldrach <daniel.drach@gmail.com>
2024-12-05 01:39:20 +0000

    Changed the file arrangement a little bit.

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+require('configs.lazy')
+require('configs.remap')
+
 vim.opt.number = true       -- Absolute line number
 vim.opt.relativenumber = true -- Relative line numbers
 
@@ -11,33 +14,3 @@ vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 
 --vim.opt.mouse = '' -- disable mouse, that makes it so you can copy through ssh, mouse interacts with the terminal, not with neovim
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = "\\"
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-      vim.api.nvim_echo({
-        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-        { out, "WarningMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
-    end
-end
-
-vim.opt.rtp:prepend(lazypath)
-
--- Setup lazy.nvim
-require("lazy").setup({
-    spec = { { import = "plugins" },}, -- searches in the ./lua/plugins folder and load all the files there
-    install = { colorscheme = { "habamax" } },-- colorscheme that will be used when installing plugins.
-    checker = { enabled = true }, -- automatically check for plugin updates
-})
-
-require('configs.remap')
