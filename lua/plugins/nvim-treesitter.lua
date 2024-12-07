122eabeffb00ed5fd10a323695e54689555fe26a
danieldrach <daniel.drach@gmail.com>
2024-12-07 01:14:00 +0000

    Added treesitter support for a couple of languages.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,36 @@ return {
     build = ":TSUpdate", -- Automatically run `:TSUpdate` after installation
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "python", "javascript", "html", "css" }, -- Specify languages
+            ensure_installed = {
+                "lua",
+                "python",
+                "javascript",
+                "html",
+                "css" ,
+                "markdown",
+                "markdown_inline",
+                "yaml",
+                "dockerfile",
+                "bash",
+                "dockerfile",
+                "gitignore",
+                "query",
+                "regex",
+                "sql",
+                "xml",
+                "zig",
+                "arduino",
+                "cpp",
+                "csv",
+                "git_config",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "json",
+                "json5",
+                "latex",
+            },
+     -- Specify languages
             highlight = {
                 enable = true, -- Enable syntax highlighting
             },
