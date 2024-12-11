deb4e62208ca15cfbc9c400fe23c21e9094cbf6e
danieldrach <daniel.drach@gmail.com>
2024-12-11 19:53:56 +0000

    Installed noice, a nevim command bar and notifications plugin.

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- Add your configuration options here
+    },
+    dependencies = {
+      -- Required dependency
+      "MunifTanjim/nui.nvim",
+      -- Optional dependency for notification view
+      "rcarriga/nvim-notify",
+    }
+  }
+}
