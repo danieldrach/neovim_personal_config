6d88c56f9e26b4e34a819ab62a283c0d30840610
danieldrach <daniel.drach@gmail.com>
2024-12-25 23:10:35 +0000

    Added ctrl+d on telescope buffer search as kill buffer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,20 @@ return {
     version = "0.1.8", -- Use the specified tag
     dependencies = { "nvim-lua/plenary.nvim" }, -- Required dependency
     config = function()
+        local actions = require("telescope.actions") 
         require("telescope").setup({
-            -- Your configuration here
+            pickers = {
+                buffers = {
+                    mappings = {
+                        i = {
+                            ["<C-d>"] = actions.delete_buffer,
+                        },
+                        n = {
+                            ["<C-d>"] = actions.delete_buffer,
+                        },
+                    },
+                },
+            },
         })
     end,
 }
