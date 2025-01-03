056ebc03e7022353db47f56756332ede6980ebf2
danieldrach <daniel.drach@gmail.com>
2025-01-03 23:05:03 +0000

    Added commentstring and snippets to openscad

--- lua/plugins/openscad.lua
+++ lua/plugins/openscad.lua
@@ -3,6 +3,14 @@ return {
     config = function()
         vim.g.openscad_load_snippets = true
         require("openscad")
+
+        -- Set commentstring for OpenSCAD
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "openscad",
+            callback = function()
+                vim.bo.commentstring = "// %s"
+            end,
+        })
     end,
     dependencies = { "L3MON4D3/LuaSnip", "junegunn/fzf.vim" },
 }
