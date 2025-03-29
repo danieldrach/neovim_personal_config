e7662c6aa6dab8c9f4286c8653f0ba9eba7ae722
danieldrach <daniel.drach@gmail.com>
2025-03-29 16:17:18 +0100

    Fixing venv-selector for a conf that works minimally in windows.

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -3,19 +3,17 @@ return {
         "linux-cultist/venv-selector.nvim",
         dependencies = {
             "neovim/nvim-lspconfig",
-            "mfussenegger/nvim-dap", 
+            "mfussenegger/nvim-dap",
             "mfussenegger/nvim-dap-python", --optional
             { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
         },
         lazy = false,
         branch = "regexp", -- This is the regexp branch, use this for the new version
         config = function()
-          require("venv-selector").setup({
-             search = {
-                patterns = { "venv", ".venv", "env", ".env" }, -- Adjust as needed
-                ignore = { "node_modules", ".git", ".cache" }, -- Ignore unnecessary dirs
-            },
-        })
+            require('venv-selector').setup({
+                search_workspace = false,
+                path = vim.fn.getcwd(),
+            })
         end,
         keys = {
           { ",v", "<cmd>VenvSelect<cr>" },
