8300e6a81814d6b02e8351f9739592f85b2b210f
danieldrach <daniel.drach@gmail.com>
2025-07-12 00:10:11 +0200

    Latest changes to avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -14,14 +14,27 @@ return {
     providers = {
         claude = {
           endpoint = "https://api.anthropic.com",
-          model = "claude-3-7-sonnet-20250219",
-          timeout = 30000,
+          -- model = "claude-3-7-sonnet-20250219",
+           model = "claude-3-5-sonnet-latest",
+          -- model = "claude-sonnet-4-0",
+          -- model = "claude-sonnet-4-20250514", -- Specific date version
+          timeout = 10000,
           extra_request_body={
               temperature = 0,
               max_tokens = 512,
-          },  
-          -- api_key = vim.env.ANTHROPIC_API_KEY,
+          },
+          api_key = vim.env.ANTHROPIC_API_KEY,
           -- disable_tools = true,
+        },
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o",
+          timeout = 10000,
+          extra_request_body = {
+            temperature = 0,
+            max_tokens = 2048,
+          },
+          api_key = vim.env.OPENAI_API_KEY,
         },
     },
   },
