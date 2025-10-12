263fdd28a9aa5c4843a17ed5f9932c00fb292198
Daniel Chaves Drach <daniel.drach@gmail.com>
2025-10-12 11:01:40 +0100

    Toggleterm now defaults to powershell when on windows

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,14 +2,31 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
+        -- Configure PowerShell as the default shell on Windows
+        if vim.fn.has("win32") == 1 then
+            local powershell_options = {
+                shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell',
+                shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+                shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+                shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+                shellquote = '',
+                shellxquote = '',
+            }
+            
+            -- Apply the PowerShell options
+            for option, value in pairs(powershell_options) do
+                vim.opt[option] = value
+            end
+        end
+        
         require("toggleterm").setup({
-            size = 90,  -- 20% of screen height for regular terminals
+            size = 90,  -- 90% of screen height for regular terminals
             open_mapping = [[<F12>]],
             direction = 'float',
             float_opts = {
                 border = 'curved',
-                width = math.floor(vim.o.columns * 0.8),  -- 80% of screen width
-                height = math.floor(vim.o.lines * 0.6),   -- 60% of screen height
+                width = math.floor(vim.o.columns * 0.9),
+                height = math.floor(vim.o.lines * 0.8),
             },
         })
     end,
