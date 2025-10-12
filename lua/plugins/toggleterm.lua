f4dfbce6414d248c13347d0ae3c76db8ca655748
danieldrach <daniel.drach@gmail.com>
2025-10-12 10:46:27 +0200

    Improved window size on toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,13 +3,13 @@ return {
     version = "*",
     config = function()
         require("toggleterm").setup({
-            size = 80,
+            size = 90,  -- 20% of screen height for regular terminals
             open_mapping = [[<F12>]],
             direction = 'float',
             float_opts = {
                 border = 'curved',
-                width = 80,
-                height = 80,
+                width = math.floor(vim.o.columns * 0.8),  -- 80% of screen width
+                height = math.floor(vim.o.lines * 0.6),   -- 60% of screen height
             },
         })
     end,
